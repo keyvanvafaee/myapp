3b57711ce8256833acf9d568858a5c54ee726c0c
keyvanvafaee <keyvankalilinux@gmail.com>
2023-03-10 19:45:45 +0330

    Updates

---
+++ precache-manifest.775075a8237307890e781894dde03625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd7f3f69f7c9ec7743d",
+    "url": "/myapp/static/js/main.1e69babf.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "1e1674f3c9b8c58b9d51",
+    "url": "/myapp/static/css/2.ce7bbd0d.chunk.css"
+  },
+  {
+    "revision": "1e1674f3c9b8c58b9d51",
+    "url": "/myapp/static/js/2.2176f148.chunk.js"
+  },
+  {
+    "revision": "ac2456cf8c7a59e7fe582373a560d930",
+    "url": "/myapp/index.html"
+  }
+];
